3b17b5af8c2db534b3d8f4890143628bc340839a
abdellah addoun <addoun.abdellah@gmail.com>
2019-06-22 11:43:20 +0100

    Creating custom dimensions section

--- src/components/home/SettingsDialog.vue
+++ src/components/home/SettingsDialog.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- Settings dialog -->
-  <v-dialog v-model="settingsDialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+  <v-dialog v-model="settingsDialog" width="900" persistent transition="dialog-bottom-transition">
     <v-card>
       <v-toolbar dark color="teal">
         <v-btn
@@ -27,8 +27,8 @@
             </v-list>
           </v-flex>
         </v-layout>
-        <v-layout row wrap>
-          <v-flex xs12 sm12 md6 lg3 px-2 pl-4>
+        <v-layout row wrap px-2>
+          <v-flex xs12 sm12 md6 lg3 px-1>
             <v-text-field
               type="number"
               label="Width"
@@ -39,7 +39,7 @@
               @blur="$v.surfaceWidth.$touch()"
             ></v-text-field>
           </v-flex>
-          <v-flex xs12 sm12 md6 lg3 px-2>
+          <v-flex xs12 sm12 md6 lg3 px-1>
             <v-text-field
               type="number"
               label="Height"
@@ -49,6 +49,17 @@
               @input="$v.surfaceHeight.$touch()"
               @blur="$v.surfaceHeight.$touch()"
             ></v-text-field>
+          </v-flex>
+          <!-- <v-flex xs12 sm12 md2 lg2 px-1>
+            <span></span>
+          </v-flex> -->
+          <v-flex xs12 sm12 md12 lg6 px-1>
+            <v-select
+              v-model="selectedItem"
+              :items="paperSizes"
+              label="Or choose from predefined sizes"
+              @change="setDimensionsToFields()"
+            ></v-select>
           </v-flex>
         </v-layout>
         <v-layout row wrap>
@@ -86,6 +97,18 @@ export default {
     }
   },
   data: () => ({
+    paperSizes: [
+      { value: "0", text: "A0", width: "841", height: "1189" },
+      { value: "1", text: "A1", width: "594", height: "841" },
+      { value: "2", text: "A2", width: "420", height: "594" },
+      { value: "3", text: "A3", width: "297", height: "420" },
+      { value: "4", text: "A4", width: "210", height: "297" },
+      { value: "5", text: "A5", width: "148", height: "210" },
+      { value: "6", text: "A6", width: "105", height: "148" },
+      { value: "7", text: "A7", width: "74", height: "105" },
+      { value: "8", text: "Set custom dimensions", width: "0", height: "0" }
+    ],
+    selectedItem: undefined,
     //? Settings dialog data
     settingsDialog: false,
     surfaceHeight: 0,
@@ -131,6 +154,10 @@ export default {
       this.surfaceHeight = window.localStorage.getItem("surfaceHeight");
       this.settingsDialog = true;
     },
+    setDimensionsToFields() {
+      this.surfaceHeight = this.paperSizes[this.selectedItem].height;
+      this.surfaceWidth = this.paperSizes[this.selectedItem].width;
+    },
     updateSurfaceDimensions() {
       this.$v.$touch();
       if (!this.$v.$invalid) {
@@ -139,8 +166,9 @@ export default {
         if (this.doShowSurfaceDimensionsAlert) {
           this.TOGGLE_SURFACE_DIMENSIONS_ALERT_STATE();
         }
-        // console.log('this.$parent :', this.$parent);
-        this.$parent.$parent.showSuccessSnackbar("Information Updated Successfully");
+        this.$parent.$parent.showSuccessSnackbar(
+          "Information Updated Successfully"
+        );
       }
     }
   }
