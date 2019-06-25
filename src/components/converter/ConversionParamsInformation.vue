1ef85c361b7b8d11eeea863e878ec6b8871cf51e
abdellah addoun <addoun.abdellah@gmail.com>
2019-06-25 14:28:54 +0100

    Adding laser params information

--- src/components/converter/ConversionParamsInformation.vue
+++ src/components/converter/ConversionParamsInformation.vue
@@ -32,26 +32,55 @@
             >The depth of the step, amount of mm that the tool should eat from the material for each step (how much tool eats per step).</v-list-tile-sub-title>
             <v-list-tile-sub-title>default: -1</v-list-tile-sub-title>
           </v-list-tile-content>
-           <v-list-tile-content>
+          <v-list-tile-content>
             <v-list-tile-title class="font-weight-medium title">White Z</v-list-tile-title>
-            <v-list-tile-sub-title class="text--primary">White pixels, value in mm corresponds to the color white.</v-list-tile-sub-title>
+            <v-list-tile-sub-title
+              class="text--primary"
+            >White pixels, value in mm corresponds to the color white.</v-list-tile-sub-title>
             <v-list-tile-sub-title>default: 0</v-list-tile-sub-title>
           </v-list-tile-content>
           <v-list-tile-content>
             <v-list-tile-title class="font-weight-medium title">Black Z</v-list-tile-title>
-            <v-list-tile-sub-title class="text--primary">Maximum depth, value in mm corresponds to the color black.</v-list-tile-sub-title>
+            <v-list-tile-sub-title
+              class="text--primary"
+            >Maximum depth, value in mm corresponds to the color black.</v-list-tile-sub-title>
             <v-list-tile-sub-title>default: -2</v-list-tile-sub-title>
           </v-list-tile-content>
           <v-list-tile-content>
             <v-list-tile-title class="font-weight-medium title">Safe Z</v-list-tile-title>
-            <v-list-tile-sub-title class="text--primary">Safe distance. the height in mm that is safe to move the tool.</v-list-tile-sub-title>
+            <v-list-tile-sub-title
+              class="text--primary"
+            >Safe distance. the height in mm that is safe to move the tool.</v-list-tile-sub-title>
             <v-list-tile-sub-title>default: 1</v-list-tile-sub-title>
           </v-list-tile-content>
           <!-- feedrate { work: (number), idle: (number) } Only the corresponding line is added. default: '', Choose which working speed to use and how fast it will move when idle -->
           <v-list-tile-content>
             <v-list-tile-title class="font-weight-medium title">Feed Rate</v-list-tile-title>
-            <v-list-tile-sub-title class="text--primary">Choose which working speed to use and how fast it will move when idle.</v-list-tile-sub-title>
+            <v-list-tile-sub-title
+              class="text--primary"
+            >Choose which working speed to use and how fast it will move when idle.</v-list-tile-sub-title>
             <!-- <v-list-tile-sub-title>default: 0.95</v-list-tile-sub-title> -->
+          </v-list-tile-content>
+          <v-list-tile-content>
+            <v-list-tile-title class="font-weight-medium title">Command Power On</v-list-tile-title>
+            <v-list-tile-sub-title
+              class="text--primary"
+            >GCode M command to turn the laser On, M3 is the perfect command</v-list-tile-sub-title>
+            <v-list-tile-sub-title>default: M3</v-list-tile-sub-title>
+          </v-list-tile-content>
+          <v-list-tile-content>
+            <v-list-tile-title class="font-weight-medium title">Spindle</v-list-tile-title>
+            <v-list-tile-sub-title
+              class="text--primary"
+            >From 100 to 1000, GCode S command to set the intencity of the laser</v-list-tile-sub-title>
+            <v-list-tile-sub-title>default: s600</v-list-tile-sub-title>
+          </v-list-tile-content>
+          <v-list-tile-content>
+            <v-list-tile-title class="font-weight-medium title">Command Power Off</v-list-tile-title>
+            <v-list-tile-sub-title
+              class="text--primary"
+            >GCode M command to turn the laser Off, M05 is the perfect command</v-list-tile-sub-title>
+            <v-list-tile-sub-title>default: M05</v-list-tile-sub-title>
           </v-list-tile-content>
         </v-list>
       </v-card-text>
